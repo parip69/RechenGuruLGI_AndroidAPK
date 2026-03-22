83a6fc20becb91c28ad47162ff4183517cb29362
parip69 <gerhardbachner@gmail.com>
2026-03-22 01:36:33 +0100

    v3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "de.parip69.rechengurulgi"
         minSdk = 24
         targetSdk = 35
-        versionCode = 2
-        versionName = "2"
+        versionCode = 3
+        versionName = "3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
